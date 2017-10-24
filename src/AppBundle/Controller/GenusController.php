5bc3c92018cef3149c031476e9f80f629196a437
Ruan Silva <ruanluiz.as@gmail.com>
2017-10-24 11:17:05 -0200

    step7 - twig: for a good time with templates

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,12 +14,17 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
 ////        return new Response("Under the sea!!!! Oh Yeah!!! <br/> The genus: ".$genusName);
 //        $templating = $this->container->get('templating');
 ////        $this->container->get('templating')
 //        $html = $templating->render('genus/show.html.twig', array('name' => $genusName));
 //
 //        return new Response($html);
-        return $this->render('genus/show.html.twig', array('name' => $genusName));
+        return $this->render('genus/show.html.twig', array('name' => $genusName, 'notes' => $notes));
     }
 }
