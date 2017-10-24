64a270a499f06956f3159fc3e35ed3df104169fd
Ruan Silva <ruanluiz.as@gmail.com>
2017-10-24 10:15:00 -0200

    step5 - intro to services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,15 +4,21 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
     {
-        return new Response("Under the sea!!!! Oh Yeah!!! <br/> The genus: ".$genusName);
+//        return new Response("Under the sea!!!! Oh Yeah!!! <br/> The genus: ".$genusName);
+        $templating = $this->container->get('templating');
+//        $this->container->get('templating')
+        $html = $templating->render('genus/show.html.twig', array('name' => $genusName));
+
+        return new Response($html);
     }
 }
