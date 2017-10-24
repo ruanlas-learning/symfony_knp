8f5ecb6d276121bfaab4a45bd6b501a3d2643ea3
Ruan Silva <ruanluiz.as@gmail.com>
2017-10-24 10:47:21 -0200

    step6 - listing and using services

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -14,11 +14,12 @@ class GenusController extends Controller
      */
     public function showAction($genusName)
     {
-//        return new Response("Under the sea!!!! Oh Yeah!!! <br/> The genus: ".$genusName);
-        $templating = $this->container->get('templating');
-//        $this->container->get('templating')
-        $html = $templating->render('genus/show.html.twig', array('name' => $genusName));
-
-        return new Response($html);
+////        return new Response("Under the sea!!!! Oh Yeah!!! <br/> The genus: ".$genusName);
+//        $templating = $this->container->get('templating');
+////        $this->container->get('templating')
+//        $html = $templating->render('genus/show.html.twig', array('name' => $genusName));
+//
+//        return new Response($html);
+        return $this->render('genus/show.html.twig', array('name' => $genusName));
     }
 }
