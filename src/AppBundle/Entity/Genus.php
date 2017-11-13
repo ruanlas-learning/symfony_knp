b8365ff9b9c93201243e82b37be9156f6435ab28
Ruan Silva <ruanluiz.as@gmail.com>
2017-11-13 11:11:19 -0200

    modulo3 - Step1 Creating an Entity Class

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wideti
+ * Date: 13/11/17
+ * Time: 10:21
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
